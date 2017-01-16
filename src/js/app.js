e9dbc9ae1e8aaefade72e0bcaf4e7f2922bb594f
Y.J <15100690644@163.com>
2017-01-16 19:12:26 +0800

    "webpack_template模板"

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+/*
+* @Author: HYJ
+* @Date:   2017-01-16 14:06:08
+* @Last Modified by:   HYJ
+* @Last Modified time: 2017-01-16 16:55:57
+*/
+import '../style/index.less'
+import '../style/indexs.css'
+
+const app = 100;
+(() =>{
+	console.log(app)
+
+})()
+const arr = [1,2,2]
+// console.log(...arr)
+ ((...arr) => {
+	console.log(arr)
+})()
+
